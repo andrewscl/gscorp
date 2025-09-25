fa0937f7a0316e8c474fd1b18d754f9deb9144a9
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-09-25 12:59:50 -0300

    Actualizar Roles

--- src/main/java/com/gscorp/dv1/controllers/UIChromeController.java
+++ src/main/java/com/gscorp/dv1/controllers/UIChromeController.java
@@ -12,35 +12,35 @@ public class UIChromeController {
     @GetMapping("/sidebar")
     public String sidebar(Authentication auth) {
         if (has(auth,"ROLE_ADMINISTRATOR"))
-            return "private/admin/fragments/admin-menu-fragment :: adminSidebar";
+            return "private/admin/fragments/admin-menu-fragment :: sidebar";
 
         if (has(auth,"ROLE_OPS"))
-            return "private/ops/fragments/ops-menu-fragment :: opsSidebar";
+            return "private/ops/fragments/ops-menu-fragment :: sidebar";
 
         if (has(auth,"ROLE_CLIENT"))
-            return "private/client/fragments/client-menu-fragment :: clientSidebar";
+            return "private/client/fragments/client-menu-fragment :: sidebar";
 
         if (has(auth,"ROLE_RRHH"))        
-            return "private/rrhh/fragments/rrhh-menu-fragment :: rrhhSidebar";
+            return "private/rrhh/fragments/rrhh-menu-fragment :: sidebar";
 
-        return "private/fragments/sidebar-empty :: sidebar";
+        return "private/default/fragments/default-menu-fragment :: sidebar";
     }
 
     @GetMapping("/topbar")
     public String topbar(Authentication auth) {
         if (has(auth,"ROLE_ADMINISTRATOR"))
-            return "private/admin/fragments/admin-menu-fragment :: adminTopbar";
+            return "private/admin/fragments/admin-menu-fragment :: topbar";
 
         if (has(auth,"ROLE_OPS"))
-            return "private/ops/fragments/ops-menu-fragment :: opsTopbar";
+            return "private/ops/fragments/ops-menu-fragment :: topbar";
 
         if (has(auth,"ROLE_CLIENT"))
-            return "private/client/fragments/client-menu-fragment :: clientTopbar";
+            return "private/client/fragments/client-menu-fragment :: topbar";
 
         if (has(auth,"ROLE_RRHH"))
-            return "private/rrhh/fragments/rrhh-menu-fragment :: rrhhTopbar";
+            return "private/rrhh/fragments/rrhh-menu-fragment :: topbar";
             
-        return "private/fragments/topbar-default :: topbar";
+        return "private/default/fragments/default-menu-fragment :: topbar";
     }
 
     private boolean has(Authentication a, String role) {
