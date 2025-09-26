48312e280f6713ab7fea6fc6cf17752b9608de7c
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-09-26 20:03:13 -0300

    Actualizar user controllers

--- src/main/java/com/gscorp/dv1/roles/web/RoleController.java
+++ src/main/java/com/gscorp/dv1/roles/web/RoleController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Controller
 @RequestMapping("/private/admin/roles")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class RoleController {
     
     @GetMapping("/create")
