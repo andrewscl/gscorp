b07dd31043dedac287e9e8252432241e2059c0b1
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-09-27 11:39:36 -0300

    Actualizar create rol

--- src/main/java/com/gscorp/dv1/roles/application/RoleService.java
+++ src/main/java/com/gscorp/dv1/roles/application/RoleService.java
@@ -1,34 +1,8 @@
 package com.gscorp.dv1.roles.application;
 
-import java.util.List;
-
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.gscorp.dv1.api.dto.RoleDto;
 import com.gscorp.dv1.entities.Role;
-import com.gscorp.dv1.roles.infrastructure.RoleRepository;
 
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class RoleService {
-
-    private final RoleRepository roleRepository;
-
-    public void saveRole (Role role){
-        role.setRole(role.getRole().toUpperCase());
-        roleRepository.save(role);
-    }
-
-    @Transactional(readOnly = true)
-    public List<RoleDto> getAllDtos(){
-        return roleRepository.findAll(Sort.by("role").ascending())
-                    .stream()
-                    .map(r -> new RoleDto(r.getId(), r.getRole()))
-                    .toList();
-    }
-
+public interface RoleService {
+    void saveRole (Role role);
+    Role findById (Long id);
 }
--- src/main/java/com/gscorp/dv1/roles/web/RoleController.java
+++ src/main/java/com/gscorp/dv1/roles/web/RoleController.java
@@ -1,19 +1,39 @@
 package com.gscorp.dv1.roles.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.AllArgsConstructor;
+import com.gscorp.dv1.roles.application.RoleService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/private/admin/roles")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RoleController {
+
+    private final RoleService roleService;
     
     @GetMapping("/create")
     public String createRoleView() {
         return "private/admin/views/create-role-view";
     }
- 
+
+    @GetMapping("/show/{id}")
+    public String showRole(@PathVariable Long id, Model model){
+        var role = roleService.findById(id);
+        model.addAttribute("role", role);
+        return "private/admin/roles/views/view-role-view";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editRole(@PathVariable Long id, Model model){
+        var role = roleService.findById(id);
+        model.addAttribute("role", role);
+        return "private/admin/roles/views/edit-role-view";
+    }
+
 }
