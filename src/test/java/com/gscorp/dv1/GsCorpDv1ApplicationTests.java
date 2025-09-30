12d81b5748d069ff66a42fdd145816235fb84cf9
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-09-30 16:31:07 -0300

    actualizar webauthn

--- src/test/java/com/gscorp/dv1/GsCorpDv1ApplicationTests.java
+++ src/test/java/com/gscorp/dv1/GsCorpDv1ApplicationTests.java
@@ -1,15 +1,5 @@
 package com.gscorp.dv1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class GsCorpDv1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
