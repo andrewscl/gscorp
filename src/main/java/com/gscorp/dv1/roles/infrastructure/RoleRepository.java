2cccff38d8cb4e7f12ecf02d1b5e7bb6c128461a
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-05 12:00:52 -0300

    webauthn

--- src/main/java/com/gscorp/dv1/roles/infrastructure/RoleRepository.java
+++ src/main/java/com/gscorp/dv1/roles/infrastructure/RoleRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.gscorp.dv1.roles.infrastructure.Role;
-
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByRole(String role);
