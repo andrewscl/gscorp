cb4ef6745e7973e0a353027adc63d493644f5524
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-06 10:08:07 -0300

    private menu movil

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
         'private/admin/users/create-user': path.resolve(__dirname, './src/private/admin/users/create-user.js'),
         'private/admin/users/edit-user': path.resolve(__dirname, './src/private/admin/users/edit-user.js'),
         'private/webauthn/webauthn-attendance': path.resolve(__dirname, './src/private/webauthn/webauthn-attendance.js'),
+        'private/menu/private-menu': path.resolve(__dirname, './src/private/menu/private-menu.js'),
       },
       output: {
         entryFileNames: '[name].js'
