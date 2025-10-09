136bb164984f7f7c031cc9eb7ff05580cfbd1b82
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-09 18:30:47 -0300

    Client

--- src/main/java/com/gscorp/dv1/clients/infrastructure/Client.java
+++ src/main/java/com/gscorp/dv1/clients/infrastructure/Client.java
@@ -1,17 +1,26 @@
 package com.gscorp.dv1.clients.infrastructure;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.gscorp.dv1.users.infrastructure.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
@@ -42,4 +51,12 @@ public class Client {
   @Builder.Default
   @Column(nullable=false)
   private Boolean active = true;   // ✅ se respeta en el builder
+
+  // Relación inversa del ManyToMany definido en User.clients
+  @Builder.Default
+  @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+  @ToString.Exclude
+  @EqualsAndHashCode.Exclude
+  private Set<User> users = new HashSet<>();
+
 }
