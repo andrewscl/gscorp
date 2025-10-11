964c9f051126cd7d761f5383ca4d2a423a5a5c31
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-11 11:48:13 -0300

    Sites

--- src/main/java/com/gscorp/dv1/sites/infrastructure/SiteRepository.java
+++ src/main/java/com/gscorp/dv1/sites/infrastructure/SiteRepository.java
@@ -3,6 +3,7 @@ package com.gscorp.dv1.sites.infrastructure;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +11,8 @@ public interface SiteRepository extends JpaRepository<Site, Long>{
 
     List<Site> findByClientId(Long clientId);
     long countByClientId(Long clientId);
+
+    @Query("SELECT s FROM Site s JOIN FETCH s.client")
+    List<Site> findAllWithClients();
     
 }
