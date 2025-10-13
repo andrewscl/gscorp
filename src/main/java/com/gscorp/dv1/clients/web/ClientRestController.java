85068b019162be1dad236f3000dfe1d6b92614a8
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-13 11:57:43 -0300

    sites & projects

--- src/main/java/com/gscorp/dv1/clients/web/ClientRestController.java
+++ src/main/java/com/gscorp/dv1/clients/web/ClientRestController.java
@@ -3,7 +3,9 @@ package com.gscorp.dv1.clients.web;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,6 +50,12 @@ public class ClientRestController {
     public ResponseEntity<List<ClientDto>> getAllClients() {
         List<ClientDto> clients = clientService.getAllClients();
         return ResponseEntity.ok(clients);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id){
+            clientService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
