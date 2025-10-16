6f4d52e9237236e073ff31c66e5c02f9827452da
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-15 22:59:01 -0300

    Google Maps

--- src/main/java/com/gscorp/dv1/controllers/UIChromeController.java
+++ src/main/java/com/gscorp/dv1/controllers/UIChromeController.java
@@ -2,7 +2,6 @@ package com.gscorp.dv1.controllers;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
