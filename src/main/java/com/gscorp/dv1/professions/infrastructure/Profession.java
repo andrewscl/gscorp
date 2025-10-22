83450e5b2651940a893e2b5fc841a7eb02a1483b
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-22 11:33:05 -0300

    employees

--- src/main/java/com/gscorp/dv1/professions/infrastructure/Profession.java
+++ src/main/java/com/gscorp/dv1/professions/infrastructure/Profession.java
@@ -18,15 +18,16 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table (name="profession")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter @Setter
 @ToString(exclude = "employees")
 @Builder
 public class Profession {
