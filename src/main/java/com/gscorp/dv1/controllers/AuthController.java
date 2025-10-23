f8c1e462cd71727ca472a3ba76e74fddde421aeb
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-22 21:56:52 -0300

    employees

--- src/main/java/com/gscorp/dv1/controllers/AuthController.java
+++ src/main/java/com/gscorp/dv1/controllers/AuthController.java
@@ -1,15 +1,9 @@
 package com.gscorp.dv1.controllers;
 
-
-import java.io.IOException;
-import java.net.URLEncoder;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -25,14 +19,6 @@ public class AuthController {
     @GetMapping("/signin")
     public String showSignInForm () {
         return "auth/views/signin-view";
-    }
-
-    @GetMapping("/define-password")
-    public String redirectToShell(@RequestParam String token,
-                        HttpServletResponse response) throws IOException {
-        String target = "/auth/define-password?token=" + URLEncoder.encode(token, "UTF-8");
-        response.sendRedirect("/shell/auth?target=" + URLEncoder.encode(target, "UTF-8"));
-        return null; // o void
     }
 
 }
