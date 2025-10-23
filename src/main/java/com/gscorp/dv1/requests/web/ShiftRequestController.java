964beabcf66a574194fab9b0d79e9cbab45d7c36
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-23 18:34:09 -0300

    password definition

--- src/main/java/com/gscorp/dv1/requests/web/ShiftRequestController.java
+++ src/main/java/com/gscorp/dv1/requests/web/ShiftRequestController.java
@@ -1,5 +1,7 @@
 package com.gscorp.dv1.requests.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gscorp.dv1.requests.application.ShiftRequestService;
+import com.gscorp.dv1.sites.application.SiteService;
+import com.gscorp.dv1.sites.web.dto.SiteDto;
 
 import lombok.AllArgsConstructor;
 
@@ -16,12 +20,20 @@ import lombok.AllArgsConstructor;
 public class ShiftRequestController {
 
     private final ShiftRequestService shiftRequestService;
+    private final SiteService siteService;
 
-        @GetMapping("/table-view")
+    @GetMapping("/table-view")
     public String getShiftRequestsTableView (Model model) {
         model.addAttribute("shiftRequests",
                     shiftRequestService.findAll());
         return "private/requests/views/shift-requests-table-view";
+    }
+
+    @GetMapping("/create")
+    public String getCreateShiftRequestView(Model model) {
+        List<SiteDto> sites = siteService.getAllSites();
+            model.addAttribute("sites", sites);
+        return "private/requests/views/create-shift-request-view";
     }
 
     @GetMapping("/show/{id}")
