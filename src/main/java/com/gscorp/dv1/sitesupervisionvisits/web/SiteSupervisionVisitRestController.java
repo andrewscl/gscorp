09c63cfc2a8d9468a1e3e1a2bebcaf2821469b08
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-10-31 14:24:32 -0300

    supervisors

--- src/main/java/com/gscorp/dv1/sitesupervisionvisits/web/SiteSupervisionVisitRestController.java
+++ src/main/java/com/gscorp/dv1/sitesupervisionvisits/web/SiteSupervisionVisitRestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -30,7 +29,6 @@ public class SiteSupervisionVisitRestController {
     private final SiteSupervisionVisitService siteSupervisionVisitService;
     
     @GetMapping("/sites")
-    @ResponseBody
     public List<SiteDto> getSitesApi() {
         return siteService.getAllSites();
     }
