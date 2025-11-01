f147a994c78942c9b73e5631b07588d3d90eb37f
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-11-01 17:31:11 -0300

    supervisors

--- src/main/java/com/gscorp/dv1/employees/infrastructure/Employee.java
+++ src/main/java/com/gscorp/dv1/employees/infrastructure/Employee.java
@@ -44,14 +44,15 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table (name="employee")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter @Setter
 @Builder
 public class Employee {
 
