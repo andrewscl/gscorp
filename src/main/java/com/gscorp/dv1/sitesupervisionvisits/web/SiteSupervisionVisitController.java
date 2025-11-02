4655a35ea0e03b060ec198ed8d10bdad21827190
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-11-02 12:07:39 -0300

    supervisors

--- src/main/java/com/gscorp/dv1/sitesupervisionvisits/web/SiteSupervisionVisitController.java
+++ src/main/java/com/gscorp/dv1/sitesupervisionvisits/web/SiteSupervisionVisitController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gscorp.dv1.sites.application.SiteService;
-//import com.gscorp.dv1.sitesupervisionvisits.application.SiteSupervisionVisitService;
+import com.gscorp.dv1.sitesupervisionvisits.application.SiteSupervisionVisitService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,13 +17,13 @@ public class SiteSupervisionVisitController {
 
     private final SiteService siteService;
     private final String googleCloudApiKey = System.getenv("GOOGLE_CLOUD_API_KEY");
-    //private final SiteSupervisionVisitService siteSupervisionVisitService;
+    private final SiteSupervisionVisitService siteSupervisionVisitService;
 
     @GetMapping("/table-view")
     public String getSiteSupervisionVisitsTableView(Model model) {
         model.addAttribute("sites", siteService.getAllSites());
         model.addAttribute("googlecloudapikey", googleCloudApiKey);
-        //model.addAttribute("siteSupervisionVisits", siteSupervisionVisitService.getAllSiteSupervisionVisits());
+        model.addAttribute("visits", siteSupervisionVisitService.getAllSiteSupervisionVisits());
         return "private/site-supervision-visits/views/site-supervision-visit-table-view";
     }
 
@@ -33,7 +33,5 @@ public class SiteSupervisionVisitController {
         model.addAttribute("googlecloudapikey", googleCloudApiKey);
         return "private/site-supervision-visits/views/create-site-sup-visit-view";
     }
-
-
 
 }
