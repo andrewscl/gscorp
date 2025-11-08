8b1d9879d2202ec1f750c83cf0cd3ea528da3717
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-11-08 13:25:06 -0300

    incidents and requirements

--- src/main/java/com/gscorp/dv1/shiftrequests/infrastructure/ShiftRequestRepository.java
+++ src/main/java/com/gscorp/dv1/shiftrequests/infrastructure/ShiftRequestRepository.java
@@ -1,11 +1,13 @@
 package com.gscorp.dv1.shiftrequests.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShiftRequestRepository extends JpaRepository<ShiftRequest, Long>{
 
+    @Query("SELECT sr.code FROM ShiftRequest sr WHERE sr.site.id = :siteId AND sr.code LIKE CONCAT(:prefix, '%') ORDER BY sr.code DESC")
     String findLastCodeBySiteIdAndPrefix(Long siteId, String prefix);
     
 }
