0e3ff225cb9ff68da5a6170adfbf644c789de544
andrewscl <42708555+andrewscl@users.noreply.github.com>
2025-12-27 12:36:08 -0300

    Employees

--- src/main/java/com/gscorp/dv1/employees/infrastructure/EmployeeRepository.java
+++ src/main/java/com/gscorp/dv1/employees/infrastructure/EmployeeRepository.java
@@ -162,7 +162,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>{
         JOIN e.bank b
         JOIN e.shiftPattern s
         JOIN e.position pos
-        JOIN e.projects proj
         WHERE e.id = :id
     """)
     Optional<EmployeeEditProjection> findEmployeeProjectionById(Long id);
