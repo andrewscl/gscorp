b36b65a7c16e089e471b952a1a06cdbaa427e6e4
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-01-17 12:21:20 -0300

    Sites Map

--- src/main/java/com/gscorp/dv1/sites/web/SiteController.java
+++ src/main/java/com/gscorp/dv1/sites/web/SiteController.java
@@ -1,13 +1,20 @@
 package com.gscorp.dv1.sites.web;
 
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gscorp.dv1.clients.application.ClientService;
+import com.gscorp.dv1.clients.web.dto.ClientSelectDto;
 import com.gscorp.dv1.projects.application.ProjectService;
 import com.gscorp.dv1.sites.application.SiteService;
+import com.gscorp.dv1.sites.web.dto.SiteDtoProjection;
+import com.gscorp.dv1.users.application.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,9 +23,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SiteController {
 
+    private final UserService userService;
     private final SiteService siteService;
-
     private final ProjectService projectService;
+    private final ClientService clientService;
 
     private String googleCloudApiKey = System.getenv("GOOGLE_CLOUD_API_KEY");
 
@@ -47,6 +55,25 @@ public class SiteController {
     @GetMapping("/set-coordinates")
     public String setSiteCoordinates(Model model) {
         model.addAttribute("sites", siteService.getAllSites());
+        model.addAttribute("googlecloudapikey", googleCloudApiKey);
+        return "private/sites/views/set-site-coordinates-view";
+    }
+
+    @GetMapping("/sites-map")
+    public String getSitesMap(
+            Model model,
+            Authentication authentication) {
+
+            Long userId = userService.getUserIdFromAuthentication(authentication);
+
+            List<ClientSelectDto> clientDtos = clientService.findClientsByUserId(userId);
+
+            List<SiteDtoProjection> siteProjections =
+                                        siteService.findSiteProjectionsByClientIds(clientDtos.stream()
+                                                                        .map(ClientSelectDto::id)
+                                                                        .toList());
+
+        model.addAttribute("sites", siteProjections);
         model.addAttribute("googlecloudapikey", googleCloudApiKey);
         return "private/sites/views/set-site-coordinates-view";
     }
