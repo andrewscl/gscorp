764770fbbe7fbf228784a40b6c8e073470caac8c
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-05-10 13:30:26 -0400

    Actualizacion Update Patrols

--- frontend/src/private/sites/sites-table.js
+++ frontend/src/private/sites/sites-table.js
@@ -1,5 +1,10 @@
+console.log('archivo cargado.');
+
 import { syncHeaderHeight } from "../../shared/sync-header-height.js";
 
+console.log('importación exitosa.');
+
 (function () {
+    console.log('syncHeaderHeight ejecución autoimvocada...');
     syncHeaderHeight('.hs-table-header', '--header-height');
 })();
