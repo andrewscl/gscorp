ceee7c1e6ddf69a2fcd873739f958bfd79891139
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-05-10 13:12:27 -0400

    Actualizacion Update Patrols

--- frontend/src/private/sites/sites-table.js
+++ frontend/src/private/sites/sites-table.js
@@ -1,5 +1,3 @@
 import { syncHeaderHeight } from "../../shared/sync-header-height.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    syncHeaderHeight('.hs-table-header', '--header-height');
-});
+syncHeaderHeight('.hs-table-header', '--header-height');
--- src/main/resources/static/js/private/sites/sites-table.js
+++ src/main/resources/static/js/private/sites/sites-table.js
@@ -1,1 +1,1 @@
-import{s as e}from"../../shared/sync-header-height.js";document.addEventListener("DOMContentLoaded",()=>{e(".hs-table-header","--header-height")});
+import{s as e}from"../../shared/sync-header-height.js";e(".hs-table-header","--header-height");
