e4b07d5e02c761b929d8e31e507e4df20546e715
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-05-25 10:51:10 -0400

    Actualizacion attendance

--- frontend/src/private/patrols/view-patrol.js
+++ frontend/src/private/patrols/view-patrol.js
@@ -1,10 +1,49 @@
 import { fetchWithAuth } from '../../auth.js';
 import { navigateTo } from '../../navigation-handler.js';
+import loadGoogleMapsAPI from '../../shared/maps/googlemaps-loader.js';
+import { initMap } from '../../shared/maps/init-map.js';
+import { addAdvancedMarker } from '../../shared/maps/advanced-marker.js';
 
 const qs  = (s) => document.querySelector(s);
 
 const backToPatrolList = () => {
     setTimeout(() => navigateTo('/private/patrols/table-view', true), 1000);
+}
+
+export const startViewMap = async () => {
+  const apiKey = googleMapsConfig.apiKey;
+
+  const id = qs('#siteId').value;
+
+  try {
+    console.log('Loading Google Maps API...');
+    await loadGoogleMapsAPI(apiKey);
+    const map = await initMap('map', {
+      mapTypeId: 'hybrid',
+      zoom: 10,
+    });
+
+    const response = await fetchWithAuth(`/api/sites/${id}`, {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' },
+    });
+
+    const siteData = await response.json();
+
+    console.log('Site data:', siteData);
+    const initialMarker = await addAdvancedMarker(map, siteData.name, siteData.lat, siteData.lon);
+
+    const bounds = new google.maps.LatLngBounds();
+    bounds.extend({ lat: parseFloat(siteData.lat), lng: parseFloat(siteData.lon) });
+    map.fitBounds(bounds);
+    map.setZoom(15);
+
+    return { map, siteData, initialMarker };
+
+  } catch (error) {
+    console.error('[site-map.js] Error al cargar la API de Google Maps:'
+                                                                    , error);
+  }
 }
 
 function bindViewPatrol() {
@@ -14,6 +53,14 @@ function bindViewPatrol() {
     }
 }
 
-(function init() {
+
+(async function init() {
+    console.log('[view-patrol init] IIFE iniciado');
+    const apiKey = googleMapsConfig.apiKey;
+
     bindViewPatrol();
+
+    await startViewMap();
+    console.log('View patrol page initialized.');
+
 })();
