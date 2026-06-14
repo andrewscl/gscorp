42f50ef0b5f7e8b5b01cac2d8a4437173769c625
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-06-14 11:55:35 -0400

    companies

--- src/main/java/com/gscorp/dv1/users/web/dto/UserDto.java
+++ src/main/java/com/gscorp/dv1/users/web/dto/UserDto.java
@@ -1,13 +1,21 @@
 package com.gscorp.dv1.users.web.dto;
 
+import java.util.UUID;
+
 import com.gscorp.dv1.roles.infrastructure.Role;
 import com.gscorp.dv1.users.infrastructure.User;
 
-public record UserDto (Long id, String username, String mail, Role role) {
+public record UserDto (
+    Long id,
+    UUID externalId,
+    String username, 
+    String mail, 
+    Role role) {
 
     public static UserDto fromEntity(User u) {
         return new UserDto(
             u.getId(),
+            u.getExternalId(),
             u.getUsername(),
             u.getMail(),
             u.getRole()
