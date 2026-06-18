7fff4284ba3709e9c61740d4fff91ea55e9a6cb0
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-06-18 09:17:53 -0400

    companies

--- src/main/java/com/gscorp/dv1/employees/infrastructure/EmployeeRepository.java
+++ src/main/java/com/gscorp/dv1/employees/infrastructure/EmployeeRepository.java
@@ -327,7 +327,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>{
             COALESCE(SUM(CASE WHEN u.status = 'EXPIRED' THEN 1 ELSE 0 END), 0L) AS expiredUsersCount,
             COALESCE(SUM(CASE WHEN u.status = 'SUSPENDED' THEN 1 ELSE 0 END), 0L) AS suspendedUsersCount
         FROM Employee e
-        INNER JOIN e.company c
+        LEFT JOIN e.company c
         GROUP BY c.name
     """)
     List<CompanyEmployeesStatProjection> getCompanyEmployeesStat();
@@ -348,8 +348,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>{
             COALESCE(SUM(CASE WHEN u.status = 'EXPIRED' THEN 1 ELSE 0 END), 0L) AS expiredUsersCount,
             COALESCE(SUM(CASE WHEN u.status = 'SUSPENDED' THEN 1 ELSE 0 END), 0L) AS suspendedUsersCount
         FROM Employee e
-        JOIN JOIN e.projects p
-        JOIN JOIN p.client cl
+        JOIN e.projects p
+        JOIN p.client cl
         LEFT JOIN e.user u
         GROUP BY cl.name
     """)
