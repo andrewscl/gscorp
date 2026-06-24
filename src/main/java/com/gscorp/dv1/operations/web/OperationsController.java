af030608f3969d89a78d47c3b6dbf3ab91c568fd
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-06-24 12:48:00 -0400

    companies

--- src/main/java/com/gscorp/dv1/operations/web/OperationsController.java
+++ src/main/java/com/gscorp/dv1/operations/web/OperationsController.java
@@ -1,9 +1,22 @@
 package com.gscorp.dv1.operations.web;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.UUID;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gscorp.dv1.attendance.application.AttendanceService;
+import com.gscorp.dv1.attendance.web.dto.DashboardHeaderInfo;
+import com.gscorp.dv1.employees.application.EmployeeService;
+import com.gscorp.dv1.security.SecurityUser;
+import com.gscorp.dv1.users.application.UserService;
+import com.gscorp.dv1.users.web.dto.UserViewDto;
 
 import lombok.AllArgsConstructor;
 
@@ -12,9 +25,53 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class OperationsController {
 
+    private UserService userService;
+    private EmployeeService employeeService;
+    private AttendanceService attendanceService;
+
     @GetMapping("/dashboard/analyst")
     public String getAnalystDashboardView(Model model) {
-        return "private/ops/dashboards/templates/analyst-dashboard-view";
+        return "private/ops/dashboards/templates/ops-analyst-dashboard";
+    }
+
+    @GetMapping("/dashboard/security-operator")
+    public String getPrivateDashboardView (
+            Model model,
+            Authentication authentication) {
+
+            if(authentication == null || !authentication.isAuthenticated()) {
+                return "redirect:/login";
+            }
+
+            Object principal = authentication.getPrincipal();
+            if(!(principal instanceof SecurityUser)) {
+                return "redirect:/login";
+            }
+
+            SecurityUser securityUser = (SecurityUser) principal;
+
+            UUID externalId = securityUser.getUser().getExternalId();
+
+            UserViewDto userViewDto = userService.findWithCompaniesAndClientsByExternalId(externalId);
+
+            var employee = employeeService.findByIdViewEmployee(userViewDto.employeeId());
+
+            LocalDateTime now = LocalDateTime.now();
+            String formattedDate = now
+                .format(DateTimeFormatter
+                .ofPattern("EEEE, dd 'de' MMMM",
+                                                new Locale("es", "ES")));
+
+            DashboardHeaderInfo dashboardHeaderInfo = attendanceService.getDashboardHeader(externalId);
+
+            model.addAttribute("employee", employee);
+            model.addAttribute("currentDate", formattedDate);
+            model.addAttribute("lastPunchText", dashboardHeaderInfo.lastPunchText());
+            model.addAttribute("nextAction", dashboardHeaderInfo.nextAction());
+            model.addAttribute("greeting", dashboardHeaderInfo.greeting());
+            model.addAttribute("emoji", dashboardHeaderInfo.emoji());
+            model.addAttribute("message", dashboardHeaderInfo.message());
+        return "private/ops/views/ops-operator-dashboard-view";
     }
 
 }
