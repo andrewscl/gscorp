23c8e05807e6e1ecddc2eff71a5699c35e90d749
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-07-29 22:32:20 -0400

    assignments

--- frontend/src/private/operations/shift-assignments/create-shift-assignment.js
+++ frontend/src/private/operations/shift-assignments/create-shift-assignment.js
@@ -14,7 +14,7 @@ const createShiftAssignment = () => {
 async function handleSiteChange() {
     const shiftRequestSelect = qs('#shiftRequestExternalId');
     const employeeSelect = qs('#employeeExternalId');
-    const siteExternalId = qs('#siteExternalId');
+    const siteExternalId = qs('#siteExternalId')?.value;
 
     console.log("-> Evento change disparado. Sitio seleccionado (UUID):", siteExternalId);
 
