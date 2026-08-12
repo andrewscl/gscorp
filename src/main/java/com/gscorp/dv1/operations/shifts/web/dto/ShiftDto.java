293c8dadc292c36bdc2393424d92e2cd9116701e
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-08-12 09:49:57 -0400

    assignments

--- src/main/java/com/gscorp/dv1/operations/shifts/web/dto/ShiftDto.java
+++ src/main/java/com/gscorp/dv1/operations/shifts/web/dto/ShiftDto.java
@@ -2,6 +2,7 @@ package com.gscorp.dv1.operations.shifts.web.dto;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import com.gscorp.dv1.enums.DayOfWeek;
@@ -14,32 +15,40 @@ public record ShiftDto (
     LocalDate shiftDate,
     OffsetDateTime startTs,
     OffsetDateTime endTs,
+    String schedule,
     ShiftStatus status,
     DayOfWeek day,
     String siteName,
     String shiftRequestCode,
-    String employeeFullName
+    String plannedEmployeeFullName
 ){
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
     public static ShiftDto fromProjection(ShiftProjection sp){
         if ( sp == null) return null;
         String name = sp.getEmployeeName();
         String surname = sp.getEmployeeFatherSurname();
         String employeeFullName = null;
         if (name != null || surname != null) {
-            employeeFullName = 
-                ((name != null ? name : "") + " " + (surname != null ? surname : "" ))
-                .trim();
+            employeeFullName = ((name != null ? name : "") + " " + (surname != null ? surname : "" )).trim();
         }
+        String schedule = "-";
+        if(sp.getStartTs() != null || sp.getEndTs() != null) {
+            schedule = sp.getStartTs().format(TIME_FORMATTER) + " - " + sp.getEndTs().format(TIME_FORMATTER);
+        }
+        DayOfWeek day = sp.getShiftDate() != null
+                        ? DayOfWeek.fromJavaTime(sp.getShiftDate().getDayOfWeek())
+                        : null;
+
         return new ShiftDto(
             sp.getId(),
             sp.getExternalId(),
             sp.getShiftDate(),
             sp.getStartTs(),
             sp.getEndTs(),
+            schedule,
             sp.getStatus(),
-            sp.getShiftDate() != null
-                ? DayOfWeek.fromJavaTime(sp.getShiftDate().getDayOfWeek())
-                : null,
+            day,
             sp.getSiteName(),
             sp.getShiftRequestCode(),
             employeeFullName
