8c6903d30ab2f75ab67de4053bee49257ac11c5e
andrewscl <42708555+andrewscl@users.noreply.github.com>
2026-08-11 23:56:06 -0400

    assignments

--- src/main/java/com/gscorp/dv1/operations/shifts/infrastructure/ShiftRepository.java
+++ src/main/java/com/gscorp/dv1/operations/shifts/infrastructure/ShiftRepository.java
@@ -124,24 +124,24 @@ public interface ShiftRepository extends JpaRepository<Shift, Long>{
                 LEFT JOIN sh.shiftRequest shr
                 LEFT JOIN sh.assignment sa
                 LEFT JOIN sa.employee e
-                WHERE   (:ignoreProjectFilter = true OR p.id IN :projectIds)
-                AND     (:startDate IS NULL OR sh.shiftDate >= :startDate)
-                AND     (:endExclusiveDate IS NULL OR sh.shiftDate < :endExclusiveDate)
-                AND     (:siteId IS NULL OR s.id = :siteId)
-                AND     (:projectId IS NULL OR p.id = :projectId)
-                AND     (:shiftStatus IS NULL OR sh.status = :shiftStatus)
+                WHERE   :ignoreProjectFilter = true OR p.id IN :projectIds
+                AND     :startDate IS NULL OR sh.shiftDate >= :startDate
+                AND     :endExclusiveDate IS NULL OR sh.shiftDate < :endExclusiveDate
+                AND     :siteId IS NULL OR s.id = :siteId
+                AND     :projectId IS NULL OR p.id = :projectId
+                AND     :shiftStatus IS NULL OR sh.status = :shiftStatus
                 """,
                 countQuery = """
                 SELECT COUNT(sh.id)
                 FROM Shift sh
                 LEFT JOIN sh.site s
                 LEFT JOIN s.project p
-                WHERE   (:ignoreProjectFilter = true OR p.id IN :projectIds)
-                AND     (:startDate IS NULL OR sh.shiftDate >= :startDate)
-                AND     (:endExclusiveDate IS NULL OR sh.shiftDate < :endExclusiveDate)
-                AND     (:siteId IS NULL OR s.id = :siteId)
-                AND     (:projectId IS NULL OR p.id = :projectId)
-                AND     (:shiftStatus IS NULL OR sh.status = :shiftStatus)
+                WHERE   :ignoreProjectFilter = true OR p.id IN :projectIds
+                AND     :startDate IS NULL OR sh.shiftDate >= :startDate
+                AND     :endExclusiveDate IS NULL OR sh.shiftDate < :endExclusiveDate
+                AND     :siteId IS NULL OR s.id = :siteId
+                AND     :projectId IS NULL OR p.id = :projectId
+                AND     :shiftStatus IS NULL OR sh.status = :shiftStatus
                 """
         )
         Page<ShiftProjection> findPageByProjectIds(
